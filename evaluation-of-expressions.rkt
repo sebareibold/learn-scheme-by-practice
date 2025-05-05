7399f712f711c8d81cda2dc3f13be3b047e1eb2c
sebareibold <sebastianreibold2003@gmail.com>
2025-05-05 20:26:57 -0300

    Uploading project

--- evaluation-of-expressions.rkt
+++ evaluation-of-expressions.rkt
@@ -1,6 +1,4 @@
 #lang racket
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
 
 ; Ejercicio (1)
 ; Puntualmente en DrRackets, cuando se introduce
